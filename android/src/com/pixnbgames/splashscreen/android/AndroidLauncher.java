85daed7f2fd77d97aaa96d32829698a70a904461
Ángel Navarro <angel80y7@gmail.com>
2015-03-04 13:20:10 +0100

    First version
    LibGDX empty project

---
+++ android/src/com/pixnbgames/splashscreen/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pixnbgames.splashscreen.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pixnbgames.splashscreen.GdxSplashScreenGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxSplashScreenGame(), config);
+	}
+}
---
+++ desktop/src/com/pixnbgames/splashscreen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pixnbgames.splashscreen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pixnbgames.splashscreen.GdxSplashScreenGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxSplashScreenGame(), config);
+	}
+}
